ea0cf7254acd0d5f3b5e1c5f75d8ae15a4bc6a84
ssingiser <singiser.sean@gmail.com>
2024-11-22 11:21:36 -0500

    first commit

---
+++ devping.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVER=172.29.200.70
+TIMES=4
+
+
+echo 'Checking pvedev1 status'
+
+ping $SERVER -c $TIMES -i &> /dev/null
+RESULT=$?
+
+if [$RESULT -eq 0 ]; then
+    echo 'System is up'
+    exit 0
+else
+    echo 'system is down'
+    exit 1
+
+fi
